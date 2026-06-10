d947f433491372a3c30ae8cb3fca605571b0def8
thanh <thanhhero765@gmail.com>
2026-06-10 09:50:29 +0700

    fix null

--- src/AeroMes.Api/OpenApi/BearerSecuritySchemeTransformer.cs
+++ src/AeroMes.Api/OpenApi/BearerSecuritySchemeTransformer.cs
@@ -10,10 +10,11 @@ internal sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvi
     public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
     {
         var schemes = await authenticationSchemeProvider.GetAllSchemesAsync();
-        if (!schemes.Any(s => s.Name == "Bearer")) return;
+        if (schemes.All(s => s.Name != "Bearer")) return;
 
         document.Components ??= new OpenApiComponents();
-        document.Components.SecuritySchemes!["Bearer"] = new OpenApiSecurityScheme
+        document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>();
+        document.Components.SecuritySchemes["Bearer"] = new OpenApiSecurityScheme
         {
             Type = SecuritySchemeType.Http,
             Scheme = "bearer",
