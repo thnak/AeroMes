095b89dc36c620e41f6a7a2ed9535bc61444baf5
thanh <thanhhero765@gmail.com>
2026-06-10 15:47:01 +0700

    refactor

--- tests/AeroMes.IntegrationTests/AuditLog/AuditLogTests.cs
+++ tests/AeroMes.IntegrationTests/AuditLog/AuditLogTests.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 using System.Net.Http.Json;
-using Xunit;
 
 namespace AeroMes.IntegrationTests.AuditLog;
 
